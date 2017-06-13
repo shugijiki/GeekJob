89a3973e293e639be70ae33914e93dcd03b75189
DESKTOP-HHEP438\guest1Day <groundcherry-001@ezweb.ne.jp>
2017-06-13 14:32:12 +0900

    Java_文字の入れ替えを提出します

---
+++ CampChallenge_java/015.標準クラス/NewClass3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.sample;
+
+
+
+/**
+ *
+ * @author guest1Day
+ */
+class NewClass3 {
+    public static void main(String[] argg){
+        String word = "きょUはぴIえIちぴIのくみこみかんすUのがくしゅUをしてIます";
+        
+        System.out.print(word.replace("U", "う").replace("I", "い"));
+    }
+    
+}
