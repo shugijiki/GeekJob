fae292bef12135a3266f1dd7618244e12e9b5a8e
DESKTOP-HHEP438\guest1Day <groundcherry-001@ezweb.ne.jp>
2017-06-13 14:31:51 +0900

    Java_文字数の取得を提出します

---
+++ CampChallenge_java/015.標準クラス/NewClass2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.sample;
+
+
+
+/**
+ *
+ * @author guest1Day
+ */
+class NewClass2 {
+    public static void main(String[] argg){
+        String word = "groundcherry002@gmail.com";
+        
+        int num = word.indexOf("@");
+        System.out.print(word.substring(num));
+    }
+    
+}
